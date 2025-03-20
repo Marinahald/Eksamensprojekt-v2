e38db5fb1f1bf85853ad276c08e0e27ace26aefa
Marinahald <159538749+Marinahald@users.noreply.github.com>
2025-03-20 16:58:03 +0100

    bla bla

--- projekt/src/main/java/com/prog/desktopPet.java
+++ projekt/src/main/java/com/prog/desktopPet.java
@@ -9,7 +9,6 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
